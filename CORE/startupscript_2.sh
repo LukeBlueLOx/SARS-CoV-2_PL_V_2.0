3094b3294cafb68f54a3a8459607265f4a4a86c6
Luke Blue <luke.blue.lox@gmail.com>
2023-01-31 07:37:51 +0100

    2023-01-31-1

--- CORE/startupscript_2.sh
+++ CORE/startupscript_2.sh
@@ -1,3 +1,7 @@
-#! /bin/bash
+#!/bin/bash
 
-python3 /app/CORE/import_csv_data_from_urls.py
+config_vals=$(python3 -c "import yaml; print(yaml.safe_load(open('config_create_sheets.yaml')))")
+MAIN=$(echo $config_vals | python3 -c "import sys; config=eval(sys.stdin.read()); print(config['MAIN'])")
+CORE=$(echo $config_vals | python3 -c "import sys; config=eval(sys.stdin.read()); print(config['CORE'])")
+
+python3 "$CORE/import_csv_data_from_urls.py"
