72b1c3273a009bbce1775a9b2a8a3cdf1553da45
Luke Blue <luke.blue.lox@gmail.com>
2023-02-01 11:05:22 +0100

    2023-02-01-11

--- startupscript1.sh
+++ startupscript1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-python3 count_sums.py
+config_vals=$(python3 -c "import yaml; print(yaml.safe_load(open('config_create_sheets.yaml')))")
+config=$(echo "$config_vals")
+MAIN=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_load(sys.stdin); print(config['MAIN'].rstrip('/'))")
+CORE=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_load(sys.stdin); print(config['CORE'].rstrip('/'))")
+
+python3 "$CORE/count_sums.py"
--- startupscript2.sh
+++ startupscript2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python3 import_csv_data_from_urls.py
+
+config_vals=$(python3 -c "import yaml; print(yaml.safe_load(open('config_create_sheets.yaml')))")
+config=$(echo "$config_vals")
+MAIN=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_load(sys.stdin); print(config['MAIN'].rstrip('/'))")
+CORE=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_load(sys.stdin); print(config['CORE'].rstrip('/'))")
+
+python3 "$CORE/import_csv_data_from_urls.py"
