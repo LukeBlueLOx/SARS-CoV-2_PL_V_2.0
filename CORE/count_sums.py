f8213289e1c50a61872a379e2624afd22221e55d
Luke Blue <luke.blue.lox@gmail.com>
2023-02-02 09:43:38 +0100

    2023-02-02-02

--- CORE/count_sums.py
+++ CORE/count_sums.py
@@ -19,10 +19,12 @@ sheet = service.spreadsheets()
 
 with open(""+str(MAIN)+"config_create_sheets.yaml", "r") as cr:
     config_vals = yaml.full_load(cr)
-SPREADSHEET_ID = config_vals['ID2']
+SPREADSHEET_ID1 = config_vals['ID2']
+SPREADSHEET_ID2 = config_vals['ID4']
+formula2 = config_vals['formula2']
 sheet_id1 = '0'
-sheet_id2 = '208518000'
 t = config_vals['datetime']
+sheet_id2 = t.strftime('%Y%m%d')
 
 a = t
 datetime = t
@@ -43,31 +45,31 @@ n = datetime - timedelta(days=13)
 
 RUN = [[str(t)]]
 request = service.spreadsheets().values().update(
-    spreadsheetId=SPREADSHEET_ID,
-    range="DVSVL!A3",
+    spreadsheetId=SPREADSHEET_ID1,
+    range="COMPUTABLE!A2",
     valueInputOption="USER_ENTERED",
     body={"values": RUN}).execute()
 print(request)
 
 request = sheet.values().clear(
-    spreadsheetId=SPREADSHEET_ID,
-    range="DVSVL!C4:C382").execute()
+    spreadsheetId=SPREADSHEET_ID1,
+    range="COMPUTABLE!E3:E382").execute()
 print(request)
 
 request = sheet.values().clear(
-    spreadsheetId=SPREADSHEET_ID,
-    range="TC!A4:A382").execute()
+    spreadsheetId=SPREADSHEET_ID1,
+    range="COMPUTABLE!F3:F382").execute()
 print(request)
 
 RUN = [
-    ["=({'"+str(n)+"'!I3}+{'"+str(m)+"'!I3}+{'"+str(l)+"'!I3}+{'"+str(k)+"'!I3}"
-     "+{'"+str(j)+"'!I3}+{'"+str(i)+"'!I3}+{'"+str(h)+"'!I3}+{'"+str(g)+"'!I3}"
-     "+{'"+str(f)+"'!I3}+{'"+str(e)+"'!I3}+{'"+str(d)+"'!I3}+{'"+str(c)+"'!I3}"
-     "+{'"+str(b)+"'!I3}+{'"+str(a)+"'!I3})/E3"]
+    ["=({'"+str(n)+"'!I2}+{'"+str(m)+"'!I2}+{'"+str(l)+"'!I2}+{'"+str(k)+"'!I2}"
+     "+{'"+str(j)+"'!I2}+{'"+str(i)+"'!I2}+{'"+str(h)+"'!I2}+{'"+str(g)+"'!I2}"
+     "+{'"+str(f)+"'!I2}+{'"+str(e)+"'!I2}+{'"+str(d)+"'!I2}+{'"+str(c)+"'!I2}"
+     "+{'"+str(b)+"'!I2}+{'"+str(a)+"'!I2})/I2"]
 ]
 request = service.spreadsheets().values().update(
-    spreadsheetId=SPREADSHEET_ID,
-    range="DVSVL!C3",
+    spreadsheetId=SPREADSHEET_ID1,
+    range="COMPUTABLE!E2",
     valueInputOption="USER_ENTERED",
     body={"values": RUN}).execute()
 print(request)
@@ -76,27 +78,27 @@ RUN = {'requests': [
     {'autoFill': {
         'range': {
             'sheetId': sheet_id1,
-            'startRowIndex': 2,
+            'startRowIndex': 1,
             'endRowIndex': 382,
-            'startColumnIndex': 2,
-            'endColumnIndex': 3,
-        },
+            'startColumnIndex': 4,
+            'endColumnIndex': 5
+        }
 
-    }},
+    }}
 
 ]}
 request = service.spreadsheets().batchUpdate(
-    spreadsheetId=SPREADSHEET_ID,
+    spreadsheetId=SPREADSHEET_ID1,
     body=RUN).execute()
 print(request)
 
 RUN = [
-    ["=({'"+str(g)+"'!H3}+{'"+str(f)+"'!H3}+{'"+str(e)+"'!H3}+{'"+str(d)+"'!H3}"
-     "+{'"+str(c)+"'!H3}+{'"+str(b)+"'!H3}+{'"+str(a)+"'!H3})/7"]
+    ["=({'"+str(g)+"'!H2}+{'"+str(f)+"'!H2}+{'"+str(e)+"'!H2}+{'"+str(d)+"'!H2}"
+     "+{'"+str(c)+"'!H2}+{'"+str(b)+"'!H2}+{'"+str(a)+"'!H2})/7"]
 ]
 request = service.spreadsheets().values().update(
-    spreadsheetId=SPREADSHEET_ID,
-    range="TC!A3",
+    spreadsheetId=SPREADSHEET_ID1,
+    range="COMPUTABLE!F2",
     valueInputOption="USER_ENTERED",
     body={"values": RUN}).execute()
 print(request)
@@ -104,83 +106,149 @@ print(request)
 RUN = {'requests': [
     {'autoFill': {
         'range': {
-            'sheetId': sheet_id2,
-            'startRowIndex': 2,
+            'sheetId': sheet_id1,
+            'startRowIndex': 1,
             'endRowIndex': 382,
-            'startColumnIndex': 0,
-            'endColumnIndex': 1,
-        },
-
-    }},
-
+            'startColumnIndex': 5,
+            'endColumnIndex': 6
+        }
+    }}
 ]}
 request = service.spreadsheets().batchUpdate(
-    spreadsheetId=SPREADSHEET_ID,
+    spreadsheetId=SPREADSHEET_ID1,
     body=RUN).execute()
 print(request)
-
-time.sleep(10)
+time.sleep(7)
 
 RUN = {'requests': [
     {'copyPaste': {
         'source': {
             'sheetId': sheet_id1,
-            'startRowIndex': 2,
+            'startRowIndex': 1,
             'endRowIndex': 382,
-            'startColumnIndex': 2,
-            'endColumnIndex': 3,
+            'startColumnIndex': 4,
+            'endColumnIndex': 6
         },
         "destination": {
             'sheetId': sheet_id1,
-            'startRowIndex': 2,
+            'startRowIndex': 1,
             'endRowIndex': 382,
-            'startColumnIndex': 2,
-            'endColumnIndex': 3,
+            'startColumnIndex': 4,
+            'endColumnIndex': 6
         },
-        "pasteType": "Paste_Values",
+        "pasteType": "Paste_Values"
 
-    }},
+    }}
 
 ]}
 request = service.spreadsheets().batchUpdate(
-    spreadsheetId=SPREADSHEET_ID,
+    spreadsheetId=SPREADSHEET_ID1,
     body=RUN).execute()
 print(request)
 
 RUN = {'requests': [
-    {'copyPaste': {
-        'source': {
-            'sheetId': sheet_id2,
-            'startRowIndex': 2,
-            'endRowIndex': 382,
-            'startColumnIndex': 0,
-            'endColumnIndex': 1,
+    {'addSheet': {
+        'properties': {
+            "sheetId": sheet_id2,
+            "title": str(t) + " - SUM",
+            "gridProperties": {
+                "columnCount": 9,
+                "rowCount": 382
+            }
         },
-        "destination": {
-            'sheetId': sheet_id2,
-            'startRowIndex': 2,
-            'endRowIndex': 382,
-            'startColumnIndex': 0,
-            'endColumnIndex': 1,
-        },
-        "pasteType": "Paste_Values",
-
     }},
-
 ]}
 request = service.spreadsheets().batchUpdate(
-    spreadsheetId=SPREADSHEET_ID,
+    spreadsheetId=SPREADSHEET_ID2,
     body=RUN).execute()
 print(request)
 
-RUN = [
-    ["=({'" + str(g) + "'!H2}+{'" + str(f) + "'!H2}+{'" + str(e) + "'!H2}+"
-     "{'" + str(d) + "'!H2}+{'" + str(c) + "'!H2}+{'" + str(b) + "'!H2}+"
-     "{'" +str(a) + "'!H2})/7"]
-]
+request = sheet.values().clear(
+    spreadsheetId=SPREADSHEET_ID2,
+    range=""+str(t)+" - SUM!A1:I382").execute()
+print(request)
+
+RUN = [[str(formula2)]]
 request = service.spreadsheets().values().update(
-    spreadsheetId=SPREADSHEET_ID,
-    range="AVRXYPL!A1",
+    spreadsheetId=SPREADSHEET_ID2,
+    range=""+str(t)+" - SUM",
     valueInputOption="USER_ENTERED",
     body={"values": RUN}).execute()
+print(request)
+time.sleep(7)
+
+RUN = {'requests': [
+    {'copyPaste': {
+        'source': {
+            'sheetId': sheet_id2,
+            'startRowIndex': 0,
+            'endRowIndex': 382,
+            'startColumnIndex': 0,
+            'endColumnIndex': 9
+        },
+        "destination": {
+            'sheetId': sheet_id2,
+            'startRowIndex': 0,
+            'endRowIndex': 382,
+            'startColumnIndex': 0,
+            'endColumnIndex': 9
+        },
+        "pasteType": "Paste_Values"
+
+    }}
+
+]}
+request = service.spreadsheets().batchUpdate(
+    spreadsheetId=SPREADSHEET_ID2,
+    body=RUN).execute()
+print(request)
+
+RUN = {"requests": [
+    {"repeatCell": {
+        "range": {
+            "sheetId": sheet_id2,
+            "startRowIndex": 1,
+            "endRowIndex": 382,
+            "startColumnIndex": 0,
+            "endColumnIndex": 1
+        },
+        "cell": {
+            "userEnteredFormat": {
+                "numberFormat": {
+                    "type": "DATE_TIME",
+                    "pattern": "yyyy-mm-dd"
+                }
+            }
+        },
+        "fields": "userEnteredFormat.numberFormat"
+    }}
+]}
+request = service.spreadsheets().batchUpdate(
+    spreadsheetId=SPREADSHEET_ID2,
+    body=RUN).execute()
+print(request)
+
+RUN = {"requests": [
+    {"repeatCell": {
+        "range": {
+            "sheetId": sheet_id2,
+            "startRowIndex": 1,
+            "endRowIndex": 382,
+            "startColumnIndex": 7,
+            "endColumnIndex": 8
+        },
+        "cell": {
+            "userEnteredFormat": {
+                "numberFormat": {
+                    "type": "DATE_TIME",
+                    "pattern": "yyyy-mm-dd"
+                }
+            }
+        },
+        "fields": "userEnteredFormat.numberFormat"
+    }}
+]}
+request = service.spreadsheets().batchUpdate(
+    spreadsheetId=SPREADSHEET_ID2,
+    body=RUN).execute()
 print(request)
