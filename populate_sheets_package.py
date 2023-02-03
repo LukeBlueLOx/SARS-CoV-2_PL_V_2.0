fd24e03b0f890cc8d81f7ba7e91f5e3a160cf68d
Luke Blue <luke.blue.lox@gmail.com>
2023-02-03 19:21:58 +0100

    2023-02-03-8

--- populate_sheets_package.py
+++ populate_sheets_package.py
@@ -4,33 +4,45 @@ from datetime import datetime, timedelta
 
 with open("config_create_sheets.yaml", "r") as cr:
     config_vals = yaml.full_load(cr)
+MAIN = config_vals['MAIN']
+CORE = config_vals['CORE']
+
+with open(""+str(MAIN)+"config_create_sheets.yaml", "r") as cr:
+    config_vals = yaml.full_load(cr)
 n = config_vals['n']
-a = datetime.today() - timedelta(days=n)
-t = a.strftime('%Y-%m-%d')
+t = datetime.today() - timedelta(days=n)
+a = t.strftime('%Y-%m-%d')
+format = "%Y-%m-%d"
+t = datetime.strptime(a, format).date()
+print(t)
 config_vals['datetime'] = t
-with open("config_create_sheets.yaml", "w") as cw:
+with open("" + str(MAIN) + "config_create_sheets.yaml", "w") as cw:
     yaml.dump(config_vals, cw, default_flow_style=True)
 
 while n > 0:
-    with open("config_create_sheets.yaml", "r") as cr:
+    with open(""+str(MAIN)+"config_create_sheets.yaml", "r") as cr:
         config_vals = yaml.full_load(cr)
     n = config_vals['n']
-    script_path = "startupscript_1.sh"
-    t = config_vals['datetime']
 
+    script_path = "" + str(MAIN) + "startupscript1.sh"
     result = subprocess.call(['bash', script_path])
 
     config_vals['n'] = n - 1
-    with open("config_create_sheets.yaml","w") as cw:
+    with open(""+str(MAIN)+"config_create_sheets.yaml", "w") as cw:
         yaml.dump(config_vals, cw, default_flow_style=True)
 
-    a = datetime.today() - timedelta(days=n)
-    t = a.strftime('%Y-%m-%d')
+    with open(""+str(MAIN)+"config_create_sheets.yaml", "r") as cr:
+        config_vals = yaml.full_load(cr)
+    n = config_vals['n']
+
+    t = datetime.today() - timedelta(days=n)
+    a = t.strftime('%Y-%m-%d')
+    format = "%Y-%m-%d"
+    t = datetime.strptime(a, format).date()
+    print(t)
     config_vals['datetime'] = t
-    with open("config_create_sheets.yaml","w") as cw:
+    with open(""+str(MAIN)+"config_create_sheets.yaml", "w") as cw:
         yaml.dump(config_vals, cw, default_flow_style=True)
 
     continue
     break
-else:
-    print("(All Operations - Successfully!)")
