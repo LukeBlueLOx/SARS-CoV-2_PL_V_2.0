bb8a2885a34f8df235770f43841c784468b55e86
Luke Blue <luke.blue.lox@gmail.com>
2023-02-04 04:42:13 +0100

    2023-02-04

--- startupscript1.sh
+++ startupscript1.sh
@@ -5,4 +5,6 @@ config=$(echo "$config_vals")
 MAIN=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_load(sys.stdin); print(config['MAIN'].rstrip('/'))")
 CORE=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_load(sys.stdin); print(config['CORE'].rstrip('/'))")
 
-python3 "$CORE/count_sums.py"
+# python3 "$CORE/import_csv_data_from_urls.py"
+# python3 "$CORE/count_sums.py"
+# python3 "$CORE/iotr.py"
--- startupscript2.sh
+++ startupscript2.sh
@@ -6,3 +6,5 @@ MAIN=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_lo
 CORE=$(echo "$config" | python3 -c "import sys; import yaml; config=yaml.safe_load(sys.stdin); print(config['CORE'].rstrip('/'))")
 
 python3 "$CORE/import_csv_data_from_urls.py"
+python3 "$CORE/count_sums.py"
+python3 "$CORE/iotr.py"
