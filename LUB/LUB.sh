46a7c7422e8cc7cf526ce0284c2b61ddd42a03b2
Luke Blue <luke.blue.lox@gmail.com>
2023-02-13 08:04:27 +0100

    2023-02-13-8

--- LUB/LUB.sh
+++ LUB/LUB.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
+python3 /home/blox_land/LUB/lub.py
+
+heroku git:clone -a scv2pl
 
 cd scv2pl
 
-git pull heroku main
-
+git update-index --chmod=+x test_startupscript.sh
 git update-index --chmod=+x startupscript1.sh
 git update-index --chmod=+x startupscript2.sh
 
