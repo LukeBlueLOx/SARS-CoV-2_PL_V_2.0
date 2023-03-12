37d4e4bee23c3e56ad04218a44a52428759116c0
Luke Blue <luke.blue.lox@gmail.com>
2023-03-12 05:02:54 +0100

    2023-03-12

--- LUB/local_startupscript.sh
+++ LUB/local_startupscript.sh
@@ -3,4 +3,4 @@
 cd /home/blox_land/PycharmProjects/SARS-CoV-2_PL_V2/
 source SARS-CoV-2_PL_V2/bin/activate
 python3 import_csv_data_from_zip_file_from_api_od.py
-python3 python3 csv_internal_sources_checker.py
+python3 csv_internal_sources_checker.py
